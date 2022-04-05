c80a4964e1bc32e664a91ebadc48284b95069cd8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-04-04 22:38:00 -0400

    [c_cdd/cst.h] Begin writing CST parser for C code; [c_cdd/CMakeLists.txt] Add `cst.h`; [c_cdd/cdd.cpp] Comment out broken code; [c_cdd/cdd.h] clang-format

--- c_cdd/cdd.cpp
+++ c_cdd/cdd.cpp
@@ -5,15 +5,33 @@ namespace emit {
 
 std::unique_ptr<clang::RecordDecl> struct_from_json_schema(/*json_schema*/) {
   auto record_decl = std::unique_ptr<clang::RecordDecl>();
+  /*clang::ASTContext C;
+  clang::DeclContext
+  clang::RecordDecl record = clang::RecordDecl::Create(const ASTContext &C,
+  TagKind TK, DeclContext *DC, SourceLocation StartLoc, SourceLocation IdLoc,
+                     IdentifierInfo *Id, RecordDecl* PrevDecl = nullptr)
+
+
+
   record_decl->setFreeStanding(true);
   record_decl->setAnonymousStructOrUnion(true);
+
+  // clang::RecordDecl::Create();
+
+  clang::IdentifierInfo *Name = nullptr;
+
+  std::string label = "your_label";  // function scope.
+      Name = &(*Context).Idents.get(label); // Use the context to get the
+  identifier. clang::LabelDecl *labelDecl = clang::LabelDecl::Create((*Context),
+  Context->getTranslationUnitDecl(),stmt->getBeginLoc(), Name);
+
 
   // Debug field content
   for (clang::RecordDecl::field_iterator jt = record_decl->field_begin();
        jt != record_decl->field_end(); ++jt) {
     llvm::outs() << jt->getType().getAsString() << " " << jt->getNameAsString()
                  << '\n';
-  }
+  }*/
   return record_decl;
 }
 
