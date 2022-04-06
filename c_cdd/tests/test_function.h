001fbbdcc8c595373c8e694eaf5563eb96d4b326
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-04-06 10:40:32 -0400

    [c_cdd/cst.h] Use `*` over `**` for `args` and `body`; add missing `*` for `struct For` fields; [c_cdd/tests/test_function.h] Finish constructing the expected output

--- c_cdd/tests/test_function.h
+++ c_cdd/tests/test_function.h
@@ -10,6 +10,7 @@ static const char sum_func_src[] = "int sum(int a, int b) { return a + b; }";
 TEST x_test_function_scanned(void) {
   const char **scanned = scanner(sum_func_src);
   ASSERT_EQ(scanned, NULL);
+  /* TODO: loop through scanned, assert contents contain expected information */
   PASS();
 }
 
@@ -20,34 +21,44 @@ TEST x_test_function_parsed(void) {
   struct Arg a_arg = {/* pos_start */ 0,
                       /* scope */ NULL,
                       /* value */ "int a",
-                      /* specifiers */ int_specifier,
+                      /* specifiers */ NULL,
                       /* name */ "a"};
   struct Arg b_arg = {/* pos_start */ 0,
                       /* scope */ NULL,
                       /* value */ "int b",
-                      /* specifiers */ int_specifier,
+                      /* specifiers */ NULL,
                       /* name */ "b"};
-  struct Arg args[] = {&a_arg, &b_arg};
+  struct Arg args[2];
   struct Return _return = {/* pos_start */ 0,
                            /* scope */ NULL,
                            /* value */ "return a + b;",
                            /* val */ "a + b"};
+  struct CstNode return_cst_node = {Return};
+  struct CstNode sum_func_body[1];
+  return_cst_node._return = _return;
+  sum_func_body[0] = return_cst_node;
 
-  const struct Function sum_func = {/* pos_start */ 0,
-                                    /* scope */ NULL,
-                                    /* value */ sum_func_src,
-                                    /* specifiers */ int_specifier,
-                                    /* name */ "sum",
-                                    /* args */
-                                    {
-                                        a_arg,
-                                        b_arg,
-                                    },
-                                    /* body */ {_return}};
-  ASSERT_EQ(parsed, NULL);
-  /* TODO: loop through scanned, assert contents contain expected information */
-  /* TODO: loop through parsed, assert contents contain expected information */
-  PASS();
+  a_arg.specifiers = int_specifier;
+  b_arg.specifiers = int_specifier;
+  args[0] = a_arg;
+  args[1] = b_arg;
+
+  {
+    struct Function sum_func = {/* pos_start */ 0,
+                                /* scope */ NULL,
+                                /* value */ sum_func_src,
+                                /* specifiers */ NULL,
+                                /* name */ "sum",
+                                /* args */ NULL,
+                                /* body */ NULL};
+    sum_func.specifiers = int_specifier;
+    sum_func.args = args;
+    sum_func.body = sum_func_body;
+    ASSERT_EQ(parsed, NULL);
+    /* TODO: loop through parsed, assert contents contain expected information
+     */
+    PASS();
+  }
 }
 
 /* Suites can group multiple tests with common setup. */
