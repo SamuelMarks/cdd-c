6842b92369183a4cd5fbabbdcf10f7123cf0ee99
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-04-07 16:05:27 -0400

    [c_cdd/cst.c] Debug scanner on multiple struct defined; [c_cdd/cst.h] Use linked-list for scanner; add `Definition`; use `Declaration` instead of `Arg` (remove `Arg`); Add C++ support; [c_cdd/tests/test_struct.h] Implement scanner tests ; [c] Init with flex/bison for C grammar (separate attempt)

---
+++ c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+extern int yyparse();
+extern FILE* yyin;
+extern int yylex();
+extern void yyerror(const char *);
+
+int main(int argc, char *argv[])
+{
+  yyin = fopen(argv[1], "r");
+  if(!yyin)
+  {
+    printf("couldn't open file for reading\n");
+    return 1;
+  }
+  /*yydebug = 1;*/
+  return yyparse();
+}
--- c_cdd/tests/test_c_cdd.c
+++ c_cdd/tests/test_c_cdd.c
@@ -5,12 +5,14 @@
 #include <greatest.h>
 
 #include "test_function.h"
+#include "test_struct.h"
 
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
   GREATEST_MAIN_BEGIN();
-  RUN_SUITE(function_suite);
+  /*RUN_SUITE(function_suite);*/
+  RUN_SUITE(struct_suite);
   GREATEST_MAIN_END();
 }
