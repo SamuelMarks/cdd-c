40be770cb80e4b15714a3e0dd53fd98d65a6402d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-16 18:27:03 -0400

    [c_cdd/{cst.c,c_cdd_utils{.h,.c},tests/test_function.h}] More work on parser (before tree-sitter rewrite)

--- c_cdd/cst.c
+++ c_cdd/cst.c
@@ -13,9 +13,8 @@ struct ScannerVars {
 
 struct ScannerVars *clear_sv(struct ScannerVars *);
 
-az_span make_slice_clear_vars(const az_span source, int32_t i,
-                              int32_t *start_index, struct ScannerVars *sv,
-                              bool always_make_expr);
+az_span make_slice_clear_vars(az_span source, int32_t i, int32_t *start_index,
+                              struct ScannerVars *sv, bool always_make_expr);
 
 struct az_span_list *scanner(const az_span source) {
   struct ScannerVars sv;
@@ -72,7 +71,6 @@ struct az_span_list *scanner(const az_span source) {
         if (!sv.in_double)
           sv.in_single = !sv.in_single;
         break;
-      case ';':
       case '\n':
         if (/*sv.lbrace == sv.rbrace || */ sv.in_cpp_comment) {
           const az_span expr =
@@ -112,23 +110,46 @@ struct az_span_list *scanner(const az_span source) {
           case '\\':
             sv.line_continuation_at = (ssize_t)i;
             break;
+          case ';':
+            if (sv.lbrace == sv.rbrace) {
+              const az_span expr =
+                  make_slice_clear_vars(source, i, &start_index, &sv,
+                      /* always_make_expr */ true);
+              if (az_span_ptr(expr) != NULL && az_span_size(expr) > 0)
+                scanned_cur_ptr = ll_push_span(&ll->size, &scanned_cur_ptr, expr);
+              break;
+            }
           case '{':
           case '}':
             /* TODO: Handle `f({{5,6}, {7,8}});` */
+            if (!sv.in_init) {
+              char *s;
+              asprintf(&s, "\nsource[%02d:%02d]        ", start_index, i - 1);
+              print_escaped_span(s, az_span_slice(source, start_index, i - 1));
+              free(s);
+              printf("sv.spaces             = %d\n"
+                     "i-start_index (%02d-%02d) = %d\n",
+                     sv.spaces, i, start_index, i - start_index);
+            }
             if (!sv.in_init) {
               /* if (i != 0 && sv.spaces != i - start_index) { */
               const az_span expr0 = make_slice_clear_vars(
                   source, i - 1, &start_index, clear_sv(&sv),
                   /* always_make_expr */ true);
-              print_escaped_span("expr0", expr0);
+
               scanned_cur_ptr =
                   ll_push_span(&ll->size, &scanned_cur_ptr, expr0);
               /*}*/
+              {
+                char *s;
+                asprintf(&s, "source[%d:%d]", start_index, i);
+                print_escaped_span(s, az_span_slice(source, start_index, i));
+                free(s);
+              }
               { /* Push just the '{' | '}' to its own node */
                 const az_span expr1 =
                     make_slice_clear_vars(source, i, &start_index, &sv,
                                           /* always_make_expr */ true);
-                print_escaped_span("expr1", expr1);
                 scanned_cur_ptr =
                     ll_push_span(&ll->size, &scanned_cur_ptr, expr1);
               }
@@ -183,7 +204,8 @@ const struct az_span_elem *tokenizer(const struct az_span_elem *const scanned) {
   struct az_span_elem *iter;
   struct az_span_elem *slices = NULL;
   struct az_span_elem **slice_cur_ptr = &slices;
-  printf("const struct str_elem *tokenizer(struct str_elem *scanned)");
+  printf("const struct az_span_elem *tokenizer(const struct az_span_elem "
+         "*const scanned)");
   putchar('\n');
 #define SUBSTR_PRINT 1
 #include "c_cdd_utils.h"
@@ -225,10 +247,23 @@ const struct az_span_elem *tokenizer(const struct az_span_elem *const scanned) {
         case ')':
         case ',': /* TODO: Handle `int a, *b` */
           if (!in_comment) {
-            if (i > 0)
+            if (i > 0) {
+              char *s;
               slice_cur_ptr =
                   ll_push_span(&ll_n, &slice_cur_ptr,
                                az_span_slice(iter->span, start_index, i - 1));
+              asprintf(&s, "[%d-1] slice_cur_ptr", i);
+              print_escaped_span(s,
+                                 az_span_slice(iter->span, start_index, i - 1));
+            }
+            {
+              char *s;
+              slice_cur_ptr =
+                  ll_push_span(&ll_n, &slice_cur_ptr,
+                               az_span_slice(iter->span, start_index, i));
+              asprintf(&s, "[%d] slice_cur_ptr", i);
+              print_escaped_span(s, az_span_slice(iter->span, start_index, i));
+            }
             slice_cur_ptr =
                 ll_push_span(&ll_n, &slice_cur_ptr,
                              az_span_slice(iter->span, start_index, i));
