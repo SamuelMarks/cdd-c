90742e13f73a5b97a32d61988c1cd92b7d83e009
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-06-05 21:31:22 -0400

    [c_cdd/ll.c] `return` over `assert`; [c_cdd/{cst.c,tests/test_ll.h}] Use `calloc` over `malloc` to fix error reading memory error (unitialised)

--- c_cdd/tests/test_ll.h
+++ c_cdd/tests/test_ll.h
@@ -6,7 +6,7 @@
 #include <ll.h>
 
 TEST x_size_t_ll() {
-  struct size_t_elem *full_ll = malloc(sizeof *full_ll);
+  struct size_t_elem *full_ll = calloc(1, sizeof *full_ll);
   struct size_t_elem **ll_cur_ptr = &full_ll, *iter;
 
   struct size_t_list *ll = malloc(sizeof *ll);
@@ -14,7 +14,7 @@ TEST x_size_t_ll() {
   const size_t l[] = {5, 6, 10, 44};
   size_t i;
 
-  ll->list = full_ll->next;
+  ll->list = full_ll->next, ll->size = 0;
 
   for (i = 0; i < sizeof l / sizeof l[0]; i++)
     size_t_list_push(&ll->size, &ll_cur_ptr, l[i]);
@@ -32,7 +32,7 @@ TEST x_size_t_ll() {
 }
 
 TEST x_az_span_ll() {
-  struct az_span_elem *full_ll = malloc(sizeof *full_ll);
+  struct az_span_elem *full_ll = calloc(1, sizeof *full_ll);
   struct az_span_elem **ll_cur_ptr = &full_ll, *iter;
 
   struct az_span_list *ll = malloc(sizeof *ll);
@@ -40,7 +40,7 @@ TEST x_az_span_ll() {
   const az_span l[] = {AZ_SPAN_FROM_STR("foo"), AZ_SPAN_FROM_STR("bar")};
   size_t i;
 
-  ll->list = full_ll->next;
+  ll->list = full_ll->next, ll->size = 0;
 
   for (i = 0; i < sizeof l / sizeof l[0]; i++)
     az_span_list_push(&ll->size, &ll_cur_ptr, l[i]);
