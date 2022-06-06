8ee48292a78960a2c737898c1af33eb43d6a687b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-06-06 19:17:33 -0400

    [c_cdd/tests/test_ll.h] No longer allocate first `elem`; [c_cdd/ll.c] Fix cleanup functions for deallocation

--- c_cdd/ll.c
+++ c_cdd/ll.c
@@ -75,13 +75,13 @@ struct size_t_elem **size_t_list_end(struct size_t_elem **size_t_elem) {
 
 struct size_t_elem **size_t_list_prepend(struct size_t_elem **size_t_elem,
                                          const size_t lu) {
-  struct size_t_elem *x = malloc(sizeof *x);
-  if (!x || !size_t_elem)
+  struct size_t_elem *new_size_t_elem = malloc(sizeof *new_size_t_elem);
+  if (!new_size_t_elem || !size_t_elem)
     return NULL;
-  x->lu = lu;
-  x->next = *size_t_elem;
-  *size_t_elem = x;
-  return &x->next;
+  new_size_t_elem->lu = lu;
+  new_size_t_elem->next = *size_t_elem;
+  *size_t_elem = new_size_t_elem;
+  return &new_size_t_elem->next;
 }
 
 struct size_t_elem **size_t_list_append(struct size_t_elem **p,
@@ -96,13 +96,17 @@ void size_t_list_push(uint32_t *ll_n, struct size_t_elem ***ll_root,
 }
 
 void size_t_elem_cleanup(struct size_t_elem **size_t_elem) {
-  if (size_t_elem == NULL)
+  if (*size_t_elem == NULL)
     return;
-  while (*size_t_elem) {
-    struct size_t_elem *cur = size_t_elem[0];
-    size_t_elem = &size_t_elem[0]->next;
-    free(cur);
+  {
+    struct size_t_elem *cur = *size_t_elem;
+    while (cur != NULL) {
+      struct size_t_elem *tmp = cur;
+      cur = cur->next;
+      free(tmp);
+    }
   }
+  *size_t_elem = NULL;
 }
 
 void size_t_list_cleanup(struct size_t_list *size_t_ll) {
@@ -118,20 +122,19 @@ void size_t_list_cleanup(struct size_t_list *size_t_ll) {
 struct az_span_elem **az_span_list_end(struct az_span_elem **span_elem) {
   assert(span_elem);
   while (*span_elem)
-    span_elem = &span_elem[0]->next;
+    span_elem = &(**span_elem).next;
   return span_elem;
 }
 
 struct az_span_elem **az_span_list_prepend(struct az_span_elem **span_elem,
                                            const az_span span) {
-  struct az_span_elem *x = malloc(sizeof *x);
-  assert(span_elem);
-  if (!x)
-    return NULL;
-  x->span = span;
-  x->next = *span_elem;
-  *span_elem = x;
-  return &x->next;
+  struct az_span_elem *new_span_elem = malloc(sizeof *new_span_elem);
+  if (!new_span_elem || !span_elem)
+    return span_elem;
+  new_span_elem->span = span;
+  new_span_elem->next = *span_elem;
+  *span_elem = new_span_elem;
+  return &new_span_elem->next;
 }
 
 struct az_span_elem **az_span_list_append(struct az_span_elem **p,
@@ -141,20 +144,23 @@ struct az_span_elem **az_span_list_append(struct az_span_elem **p,
 
 void az_span_list_push(uint32_t *ll_n, struct az_span_elem ***ll_root,
                        const az_span span) {
-  if (az_span_ptr(span) != NULL && az_span_size(span) > 0) {
-    (*ll_n)++;
-    *ll_root = az_span_list_append(*ll_root, span);
-  }
+  (*ll_n)++;
+  *ll_root = az_span_list_append(*ll_root, span);
 }
 
 void az_span_elem_cleanup(struct az_span_elem **az_span_element) {
   if (az_span_element == NULL)
     return;
-  while (*az_span_element) {
-    struct az_span_elem *cur = az_span_element[0];
-    az_span_element = &az_span_element[0]->next;
-    free(cur);
+
+  {
+    struct az_span_elem *cur = *az_span_element;
+    while (cur != NULL) {
+      struct az_span_elem *tmp = cur;
+      cur = cur->next;
+      free(tmp);
+    }
   }
+  *az_span_element = NULL;
 }
 
 void az_span_list_cleanup(struct az_span_list *size_t_ll) {
