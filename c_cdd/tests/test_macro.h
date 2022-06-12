56554dc890559e96026d90e4004bfa85e1ee9baa
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-06-11 23:40:17 -0400

    [c_cdd/scanner_types.c] Implement and expose `StrScannerKind`; [c_cdd/tests/{test_literal_str.h,test_comment.h,test_macro.h,test_struct.h}] Use `StrScannerKind` to match test mock input `str` with `kind`

--- c_cdd/tests/test_macro.h
+++ c_cdd/tests/test_macro.h
@@ -21,9 +21,10 @@ TEST x_test_macro_scanned(void) {
   struct scan_az_span_elem *iter;
   enum { n = 3 };
   size_t i;
-  static const char *scanned_str_l[n] = {"# define foo bar\n", "#ifdef FOO\n",
-                                         "# define CAT(bar,foo)(bar ## foo)\n"};
-  static enum ScannerKind scanned_kind_l[n] = {Macro, Macro, Macro};
+  struct StrScannerKind scanned_l[n] = {
+      {"# define foo bar\n", Macro},
+      {"#ifdef FOO\n", Macro},
+      {"# define CAT(bar,foo)(bar ## foo)\n", Macro}};
 
   ASSERT_EQ(scanned->size, n);
 
@@ -32,10 +33,11 @@ TEST x_test_macro_scanned(void) {
     const int32_t n = az_span_size(iter->span) + 1;
     char *iter_s = malloc(n);
     az_span_to_str(iter_s, n, iter->span);
-    ASSERT_STR_EQ(iter_s, scanned_str_l[i]);
-    ASSERT_EQ(iter->kind, scanned_kind_l[i]);
+    ASSERT_STR_EQ(scanned_l[i].s, iter_s);
+    ASSERT_EQ(scanned_l[i].kind, iter->kind);
     free(iter_s);
   }
+  ASSERT_EQ(scanned->size, i);
   scan_az_span_list_cleanup(scanned);
   ASSERT_EQ(scanned->size, 0);
   ASSERT_EQ(scanned->list, NULL);
