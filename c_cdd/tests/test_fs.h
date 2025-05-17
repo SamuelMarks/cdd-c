ea2a63e15b243e2f98ce0142f718da627ac0e381
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-05-16 19:53:03 -0600

    [README.md] Add more reasons to use C ; [c_cdd/tests/test_fs.h] Fix test on MSVC

--- c_cdd/tests/test_fs.h
+++ c_cdd/tests/test_fs.h
@@ -4,16 +4,25 @@
 #include <fs.h>
 #include <greatest.h>
 
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+#define PATH_SEP "\\"
+#define PATH_SEP_C '\\'
+#define strtok_r strtok_s
+#else
+#define PATH_SEP "/"
+#define PATH_SEP_C '/'
+#endif /* defined(_MSC_VER) && !defined(__INTEL_COMPILER) */
+
 TEST test_get_basename(void) {
   const char *res;
 
-  res = get_basename("/foo/bar/baz.txt");
+  res = get_basename(PATH_SEP "foo" PATH_SEP "bar" PATH_SEP "baz.txt");
   ASSERT_STR_EQ("baz.txt", res);
 
   res = get_basename("file.txt");
   ASSERT_STR_EQ("file.txt", res);
 
-  res = get_basename("/foo/bar/");
+  res = get_basename(PATH_SEP "foo" PATH_SEP "bar" PATH_SEP);
   ASSERT_STR_EQ("", res);
 
   res = get_basename(NULL);
