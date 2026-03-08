684be8d6c773b17c5daa2a830acfb33d5949cbc3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-03-08 23:25:29 +1300

    More build types and fixes for Windows environments

--- test_msvc2022.bat
+++ test_msvc2022.bat
@@ -1,3 +1,6 @@
+@echo off
+mkdir build_test_msvc2022
+cd build_test_msvc2022
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G Ninja -DC_CDD_BUILD_TESTING=ON ..
 cmake --build .
--- test_msvc2026.bat
+++ test_msvc2026.bat
@@ -1,3 +1,6 @@
+@echo off
+mkdir build_test_msvc2026
+cd build_test_msvc2026
 call "C:\Program Files\Microsoft Visual Studio\18\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G Ninja -DC_CDD_BUILD_TESTING=ON ..
 cmake --build .
