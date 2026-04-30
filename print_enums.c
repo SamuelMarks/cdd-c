a23c272ae014df12feea4f547598289210bd0240
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-04-30 19:53:13 +1200

    Increase coverage

--- print_enums.c
+++
@@ -1,7 +0,0 @@
-#include "src/classes/parse/cdd_token.h"
-#include <stdio.h>
-int main() {
-  printf("LPAREN=%d RPAREN=%d LBRACKET=%d RBRACKET=%d OTHER=%d\n",
-         CDD_TOKEN_LPAREN, CDD_TOKEN_RPAREN, CDD_TOKEN_LBRACKET,
-         CDD_TOKEN_RBRACKET, CDD_TOKEN_OTHER);
-}
