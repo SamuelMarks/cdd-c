aca33d4b4d097910e609919526a1912a11483f91
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-05-16 10:09:57 +1200

    Update state and resolve untracked/generated files

---
+++ c_cddConfig.h
@@ -0,0 +1,11 @@
+#ifndef C_CDD_CONFIG_H
+#define C_CDD_CONFIG_H
+
+#define C_CDD_VERSION_MAJOR 0
+#define C_CDD_VERSION_MINOR 0
+#define C_CDD_VERSION_PATCH 1
+#define C_CDD_VERSION       "0.0.1"
+
+#define _ARM_
+
+#endif /* !C_CDD_CONFIG_H */
