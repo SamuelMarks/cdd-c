b7a683a20e4238720077f0090f958d915d48e03a
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-07-30 10:37:50 +1200

    fix: properly add missing mock symbols for crypto tests to unbreak link

--- src/g_fail_io_after.c
+++ src/g_fail_io_after.c
@@ -1,3 +1,12 @@
 #include "c_cdd_export.h"
 C_CDD_EXPORT int g_fail_io_after = -1;
 C_CDD_EXPORT int g_io_calls = 0;
+
+int g_crypto_fail_digestfinal = 0;
+int g_crypto_fail_digestfinal_len = 0;
+int g_crypto_fail_digestinit = 0;
+int g_crypto_fail_digestupdate = 0;
+int g_crypto_fail_hmac = 0;
+int g_crypto_fail_hmac_len = 0;
+int g_crypto_fail_mdctx_new = 0;
+int g_crypto_fail_sha256 = 0;
